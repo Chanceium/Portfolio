e64dbf81258c3e0918b45652da640571ad473d58
Chanceium <therezint@gmail.com>
2025-03-03 12:41:11 -0500

    fixing compose

--- deploy_portfolio.sh
+++ deploy_portfolio.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-# Stop the running container
-#sudo docker container stop portfolio
-
-# Remove the old image
-sudo docker image rm viteportfolio -f
-
-# Build the new image
 sudo docker build -t viteportfolio .
-
-# Run the new container
-#sudo docker run -d --rm -p 5173:8080 --name portfolio viteportfolio
